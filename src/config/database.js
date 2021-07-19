1fbb870a5fcb3a0f44f4d1f7facf317d014ad1f0
Gabriel Brum <gabriel.brum@anestech.com.br>
2021-07-19 17:43:30 -0300

    fix db host

--- src/config/database.js
+++ src/config/database.js
@@ -2,10 +2,6 @@ require('dotenv').config({
   path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
 });
 
-if (process.env.NODE_ENV === 'production') {
-  // process.env.DB_HOST = process.env.DATABASE_URL
-}
-
 module.exports = {
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
